689be2e26f0e5bba468be7c52a819de355245bf9
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-11-05 13:18:20 -0300

    Revert "Criando a camada service de Bairro, Cidade, Cliente, Colaborador, EnderecoCliente, EnderecoColaborador, Genero, Logradouro, TelefoneCliente. Erros corrigidos."
    This reverts commit 0ab6331d4bb92456194bbccceef5e5c44e46b1cc.

--- Sprint4_DDD/src/main/java/org/example/resources/TelefoneClienteResource.java
+++ Sprint4_DDD/src/main/java/org/example/resources/TelefoneClienteResource.java
@@ -5,7 +5,6 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.example.models.TelefoneCliente;
 import org.example.models.repositories.TelefoneClienteRepository;
-import org.example.services.TelefoneClienteService;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -13,25 +12,27 @@ import java.util.Optional;
 
 @Path("/telefone-cliente")
 public class TelefoneClienteResource {
-    private TelefoneClienteService service = new TelefoneClienteService();
+    private TelefoneClienteRepository repository = new TelefoneClienteRepository();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAll() throws SQLException {
-        return service.getAllService();
+    public List<TelefoneCliente> getAll() throws SQLException {
+        return repository.findAll();
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getById(@PathParam("id") int id) throws SQLException {
-        return service.getByIdService(id);
+    public TelefoneCliente getById(@PathParam("id") int id) throws SQLException {
+        TelefoneCliente telefoneCliente = repository.find(id).orElse(null);
+        return telefoneCliente;
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response insert(TelefoneCliente telefoneCliente) throws SQLException {
-        return service.insertService(telefoneCliente);
+        repository.add(telefoneCliente);
+        return Response.status(Response.Status.CREATED).build();
     }
 
     @PUT
@@ -39,13 +40,24 @@ public class TelefoneClienteResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response update(@PathParam("id") int id, TelefoneCliente telefoneCliente) throws SQLException {
-        return service.updateService(id, telefoneCliente);
+        if (repository.find(id).isPresent()) {
+            telefoneCliente.getCliente().setId(id);
+            repository.update(telefoneCliente);
+            Optional<TelefoneCliente> telefoneClienteAtualizado = repository.find(id);
+            return Response.status(Response.Status.OK).entity(telefoneClienteAtualizado).build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).entity(telefoneCliente).build();
     }
 
     @DELETE
     @Path("{id}")
     public Response delete(@PathParam("id") int id) throws SQLException {
-        return service.deleteService(id);
+        if (repository.find(id).isPresent()) {
+            repository.delete(id);
+            return Response.status(Response.Status.OK).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 
 }
